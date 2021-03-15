7cd7d1cabf66f9f09d7e54017489ed6af35cdfe8
illiakovtun <illia.kovtun.work@gmail.com>
2021-03-15 15:03:21 +0200

    range button pagination

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,29 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Chart.module.scss";
 import LineGraph from "./LineGraph";
 
 
 const Chart = (props) => {
+	const [chosenRange, setChosenRange] = useState()
 
-  return (
-    <div className={style.chart}>
-      <div className={style.head}>
-        <div className={style.title}>Performance chart</div>
-        <div className={style.date}>
-          <div className={style.active}>ALL</div>
-          <div>1D</div>
-          <div>1W</div>
-          <div>1M</div>
-          <div>3M</div>
-          <div>6M</div>
-        </div>
-      </div>
-      <div className={style.content}>
-        <div className={style.line}>
-          <LineGraph width={"1000px"} height={"350px"}/>
-        </div>
-      </div>
-    </div> 
-  );
+	const ranges = ["ALL", "1D", "1W", "1M", "3M", "6M"]
+
+
+	const rangeHandler = (e) => {
+		e.target.classList.add(style.active)
+		setChosenRange(e.target.innerText)
+	}
+
+
+	return (
+		<div className={style.chart}>
+			<div className={style.head}>
+				<div className={style.title}>Performance chart</div>
+				<div className={style.date}>
+					{
+						ranges.map(range => <div
+							key={range} onClick={(e) => rangeHandler(e)}
+							className={chosenRange === range ? style.active : style.ranges}
+						>{range}</div>)
+					}
+				</div>
+			</div>
+			<div className={style.content}>
+				<div className={style.line}>
+					<LineGraph
+						width={"1000px"}
+						height={"350px"}
+						chosenRange={chosenRange}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 export default Chart;
